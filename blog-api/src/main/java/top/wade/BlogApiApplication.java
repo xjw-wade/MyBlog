9f7e388a00ce9b2408c01ae20d0135808e02d437
xjw-wade <1955925025@qq.com>
2023-06-16 19:49:32 +0800

    fix ignore

---
+++ blog-api/src/main/java/top/wade/BlogApiApplication.java
@@ -0,0 +1,11 @@
+package top.wade;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogApiApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(BlogApiApplication.class, args);
+    }
+}
