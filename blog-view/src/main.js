dd112c3a5d19396a990f65d6ebf6f7216f1933a1
xjw-wade <1955925025@qq.com>
2023-06-17 16:22:43 +0800

    fix log ignore

--- blog-view/src/main.js
+++ blog-view/src/main.js
@@ -23,7 +23,7 @@ import Viewer from 'v-viewer'
 import './util/directive'
 
 console.log(
-	'%c NBlog %c By Naccl %c https://github.com/Naccl/NBlog',
+	'%c NBlog %c By Wade %c https://github.com/Naccl/NBlog',
 	'background:#35495e ; padding: 1px; border-radius: 3px 0 0 3px;  color: #fff',
 	'background:#41b883 ; padding: 1px; border-radius: 0 3px 3px 0;  color: #000',
 	'background:transparent'
